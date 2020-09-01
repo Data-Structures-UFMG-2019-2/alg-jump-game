1c6e10807e6eaa39cd29585014da15b2994ee823
LuizPPA <luizphilippep@gmail.com>
2020-09-01 17:07:14 -0300

    Time recording adjustments

--- time.sh
+++ time.sh
@@ -5,7 +5,7 @@ INPUT_GENERATOR=$3
 for i in {1..100..1}; do
     for j in {1..10..1}; do
         testname=$(printf "%02d_tmp" $i)
-        $INPUT_GENERATOR $i $i 11 input/$testname.in timed
+        $INPUT_GENERATOR $i $i 11 input/$testname.in
         $EXEC input/$testname.in timed > $TMP_OUT
     done
 done
